812e67f0a933ef82b39de27a38eb1e62ded659b6
Martin Taylor <contact@martintaylor.dev>
2026-04-19 14:26:18 +0100

    Initial Commit

--- SecurityCentreEngine/src/main/java/dev/martintaylor/securitycentreengine/SecurityCentreEngineApplication.java
+++ SecurityCentreEngine/src/main/java/dev/martintaylor/securitycentreengine/SecurityCentreEngineApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityCentreEngineApplication {
-
-    public static void main(String[] args) {
+class SecurityCentreEngineApplication {
+    static void main(String[] args) {
         SpringApplication.run(SecurityCentreEngineApplication.class, args);
     }
-
 }
